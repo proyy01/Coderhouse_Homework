91f49d977a76951ec8bf00805c29a89e5d93e748
proyy01 <benjamin.rozas2001@gmail.com>
2024-07-24 19:05:35 -0400

    Structure of the second activity is done

--- 2_Segunda_Pre-Entrega/main.py
+++ 2_Segunda_Pre-Entrega/main.py
@@ -2,3 +2,6 @@
 
 # Instanciar objeto
 
+# Darle acciones al objeto
+
+# Podemos manejar todo lo anterior en un menu
---
+++ 2_Segunda_Pre-Entrega/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "paquete1",
+    version = "1.0",
+    description = "Paquete de segunda pre-entrega",
+    author = "Benjamin Rozas",
+    author_email = "benjamin.rozas2001@gmail.com",
+
+    packages = ["paquete1"]
+)
