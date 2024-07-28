620e5cd0ac7ebeba149856c8e90b80852daa349d
proyy01 <benjamin.rozas2001@gmail.com>
2024-07-27 21:10:02 -0400

    Second activity/homework completed!

--- 2_Segunda_Pre-Entrega/main.py
+++ 2_Segunda_Pre-Entrega/main.py
@@ -1,10 +1,39 @@
-# Importar los modulos
-from paquete1 import modulo1, modulo2
+#Importar módulos
+from paquete1.modulo1 import *
+from paquete1.modulo2 import Cliente
 
-# Instanciar objeto
+#Instanciar objeto
+mi_cliente = Cliente("Lord Baldomero", "2 de Agosto de 1992", "Escuela Taratowarts", 9543123)
 
+#Darle acciones al objeto (Estos son solo ejemplos)
+# mi_cliente.agregar("Cartas Magicas")
+# mi_cliente.pagar()
+# print(mi_cliente)
 
-# Darle acciones al objeto
+#Menú
+def menu():
+    #Esta función abre el menú, el cual ofrece las opciones que el usuario necesite.
+    while True:
+        print("1. Agregar artículo al carrito de compras")
+        print("2. Pagar carrito de compras")
+        print("3. Mostrar detalles del cliente")
+        print("4. Salir")
+        try:
+            opción = int((input("Escoga la opción: ")))
+        except:
+            print("No has seleccionado una opción, intente de nuevo.")
+            continue
+        if opción == 1:
+            articulo = input("Indique el producto que desee agregar: ")
+            print(mi_cliente.agregar(articulo))
+        elif opción == 2:
+            print(mi_cliente.pagar())
+        elif opción == 3:
+            print(mi_cliente)
+        elif opción == 4:
+            print("Hasta la vista, baby...")
+            break
+        else:
+            print("No has seleccionado una opción, intente de nuevo.")
 
-
-# Podemos manejar todo lo anterior en un menu
+menu()
--- 2_Segunda_Pre-Entrega/paquete1/modulo2.py
+++ 2_Segunda_Pre-Entrega/paquete1/modulo2.py
@@ -1,18 +1,20 @@
 class Cliente:
-    def __init__(self, nombre, fecha_de_nacimiento, dirección, teléfono):
+    def __init__(self, nombre, fecha_de_nacimiento, direccion, telefono):
         self.nombre = nombre
         self.fecha_de_nacimiento = fecha_de_nacimiento
-        self.dirección = dirección
-        self.teléfono = teléfono
+        self.direccion = direccion
+        self.telefono = telefono
     
-        def agregar(self): #Agregar Carrito
-            artículo = input("Indique el producto que desee agregar:")
-            print(f"Se ha agregado {artículo} al carrito de compras.")
+    def agregar(self, articulo): #Agregar Carrito
+            return f"Se ha agregado {articulo} al carrito de compras."
 
-        def pagar(self): #Pagar
-            print("Se han pagado los artículos dentro del carrito de compras.")
+    def pagar(self): #Pagar
+            return "Se han pagado los artículos dentro del carrito de compras."
 
-        def __str__(self):
-            return f"Nombre:{self.nombre}, Fecha de nacimiento:{self.fecha_de_nacimiento}, Dirección:{self.dirección}, Teléfono:{self.teléfono}"
-        
-cliente = Cliente("Lord Baldomero", "Escuela Taradowarts", "+56 9 1234 5678", "Cartas de magia")
+    def __str__(self):
+            return (f"Nombre: {self.nombre}\n"
+                    f"Fecha de Nacimiento: {self.fecha_de_nacimiento}\n"
+                    f"Dirección: {self.direccion}\n"
+                    f"Teléfono: {self.telefono}")
+    
+mi_cliente = Cliente("Lord Baldomero", "2 de Agosto de 1992", "Escuela Taratowarts", 9543123)
